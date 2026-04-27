2babe8dfa9a70e1f061db0200c44b374970be91f
oOKaragaOo <102192098+oOKaragaOo@users.noreply.github.com>
2026-04-27 10:50:37 +0700

    refactor(remove) DebugRount.tx

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -5,7 +5,6 @@ import airlineRoutes from './airlineRoutes';
 import destinationRoutes from './destinationRoutes';
 import statisticsRoutes from './statisticsRoutes';
 import healthRoutes from './healthRoutes';
-import debugRoutes from './debugRoutes';
 
 const router = Router();
 
@@ -16,7 +15,6 @@ router.use('/airlines', airlineRoutes);
 router.use('/destinations', destinationRoutes);
 router.use('/statistics', statisticsRoutes);
 router.use('/health', healthRoutes);
-router.use('/debug', debugRoutes);
 
 export default router;
 
